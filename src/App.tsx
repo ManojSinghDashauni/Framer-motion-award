55861c96cd84ef4aa5dbc04339e6c7c4d46a424a
manoj singh dashauni <dashauni007@gmail.com>
2025-01-08 18:08:51 +0530

    Awwwards Remake | Menu 3D Stack Effect Using React & CSS - webunlocked

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import { Home } from "./component"
+import { Link, Route, Routes } from "react-router";
+import { Home } from "./component";
 
 const App = () => {
   return (
     <div>
-      <Home/>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+      </Routes>
+      <nav className="p-4 uppercase flex justify-center">
+        <Link
+          to="/home"
+          className="font-bold hover:text-red-400 font-serif"
+        >
+          stack nav
+        </Link>
+      </nav>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
